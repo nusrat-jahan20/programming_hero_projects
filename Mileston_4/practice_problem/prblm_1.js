31450a7822b46ee66aa9fb6ecfefaaa033c41937
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-01-27 19:16:09 +0600

    added two js file

---
+++ Mileston_4/practice_problem/prblm_1.js
@@ -0,0 +1,5 @@
+var money = 1000;
+var cost = 700;
+var rest_money = money - cost;
+
+console.log(rest_money);
---
+++ Mileston_4/practice_problem/prblm_2.js
@@ -0,0 +1,9 @@
+var math = 75.25;
+var bio = 65;
+var chem = 80;
+var phy = 35.45;
+var bang = 99.50; 
+
+var average = (math + bio + chem + phy + bang) / 5;
+average = average.toFixed(2);
+console.log(average);
