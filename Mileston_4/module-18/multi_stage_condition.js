ac4228d58d3847ed0a5013b05bfade37ebc341fd
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-01-31 00:43:14 +0600

    added practice_prblm_1.js

---
+++ Mileston_4/module-18/multi_stage_condition.js
@@ -0,0 +1,17 @@
+var money = 40;
+var danishPrice = 45;
+var butterbon = 35;
+var toastBiscuit = 20;
+
+if(money >= danishPrice) {
+    console.log('Danish Khabo');
+}
+else if(money >= butterbon) {
+    console.log('Butterbon Khabo');
+}
+else if(money >= toastBiscuit) {
+    console.log('Toast biscuit khabo');
+}
+else {
+    console.log('cha sasther jnno valo');
+}
---
+++ Mileston_4/module-18/practice_prblm/prblm_1.js
@@ -0,0 +1,17 @@
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+// index of banana
+var index = fruits.indexOf('Banana');
+console.log(index);
+
+// replacing banana with mango
+fruits[1] = 'Mango'
+console.log(fruits);
+
+// remove orange
+fruits.pop();
+console.log(fruits);
+
+// add watermelon
+fruits.push('Watermelon');
+console.log(fruits);
