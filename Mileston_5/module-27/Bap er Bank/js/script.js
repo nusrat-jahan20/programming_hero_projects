a513316333b08fd7b9980c02e2e093ac5c77b1d2
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-02-24 10:18:37 +0600

    attached bakn.html file

--- Mileston_5/module-27/Bap er Bank/js/script.js
+++ Mileston_5/module-27/Bap er Bank/js/script.js
@@ -1,1 +1,24 @@
 // step-1
+document.getElementById('submit-btn').addEventListener('click', function() {
+    // use .value to get text from any input field
+    // step-2: get email address
+    // 2.a: set id on the html element
+    // 2.b: get the element
+    // 2.c: get the value from the element
+    const emailField = document.getElementById('user-email');
+    const emailValue = emailField.value;
+  
+    // step-3: get password
+    // follow same steps of email field
+    const passwordField = document.getElementById('user-password');
+    passwordValue = passwordField.value;
+
+    // DANGER: DO NOT VERIFY email / password on the client side
+    // step-4: verify email and password
+    if(emailValue === 'nusrat.jahan99@gmail.com' && passwordValue === 'secret2000') {
+        console.log('valid user');
+    }
+    else {
+        console.log('invalid user')
+    }
+})
