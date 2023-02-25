86c79b2b259f4b1f7116d6e689fa50689b51ee36
nusrat-jahan20 <nusrat.jahan.tasnim20@gmail.com>
2023-02-25 19:42:43 +0600

    added problem-1 js file

--- Mileston_5/module-27/Bap er Bank/js/deposite.js
+++ Mileston_5/module-27/Bap er Bank/js/deposite.js
@@ -3,29 +3,35 @@ document.getElementById('deposite-btn').addEventListener('click', function() {
     const depositeField = document.getElementById('deposite-field');
     const depositeFieldValueString = depositeField.value;
     const depositeFieldValue = parseFloat(depositeFieldValueString);
+
+    // step-2: clear input field
+    depositeField.value = ''
+
+     // extra step: error handling
+    if(isNaN(depositeFieldValue)) {
+        alert('please provide a valid number');
+        return;
+    }
     
-    // step-2: get the value of deposite amount and convert it to float type
+    // step-3: get the value of deposite amount and convert it to float type
     const depositeAmount = document.getElementById('deposite-amount');
     const depositeAmountValueString = depositeAmount.innerText;
     const depositeAmountValue = parseFloat(depositeAmountValueString);
 
-    // step-3: add input field value and deposite amount
-    const totalDepositeAmount = depositeFieldValue + depositeAmountValue;
-
-    // step-4: update current deposite amount
-    depositeAmount.innerText = totalDepositeAmount;
-
-    // step-5: get the value of balance amount and convert it to float type
+    // step-4: get the value of balance amount and convert it to float type
     const balanceAmount = document.getElementById('balance-amount');
     const balanceAmountValueString = balanceAmount.innerText;
     const balanceAmountValue = parseFloat(balanceAmountValueString);
 
-    // step-6: add input field value and balance amount
+    // step-5: add input field value and deposite amount
+    const totalDepositeAmount = depositeFieldValue + depositeAmountValue;
+
+    // step-6: update current deposite amount
+    depositeAmount.innerText = totalDepositeAmount;
+
+    // step-7: add input field value and balance amount
     const totalBalanceAmount = depositeFieldValue + balanceAmountValue;
 
-    // step-7: update current balance amount
+    // step-8: update current balance amount
     balanceAmount.innerText = totalBalanceAmount;
-
-    // step-8: clear input field
-    depositeField.value = ''
 })
--- Mileston_5/module-27/Bap er Bank/js/withdraw.js
+++ Mileston_5/module-27/Bap er Bank/js/withdraw.js
@@ -4,28 +4,43 @@ document.getElementById('withdraw-btn').addEventListener('click', function() {
     const withdrawFieldValueString = withdrawField.value;
     const withdrawFieldValue = parseFloat(withdrawFieldValueString);
     
-    // step-2: get the value of deposite amount and convert it to float type
+    // step-2: clear input field
+    withdrawField.value = '';
+
+     // extra step: error handling
+    if(isNaN(withdrawFieldValue)) {
+        alert('please provide a valid number');
+        return;
+    }
+
+    // step-3: get the value of deposite amount and convert it to float type
     const withdrawAmount = document.getElementById('withdraw-amount');
     const withdrawAmountValueString = withdrawAmount.innerText;
     const withdrawAmountValue = parseFloat(withdrawAmountValueString);
 
-    // step-3: add input field value and deposite amount
-    const totalwithdrawAmount = withdrawFieldValue + withdrawAmountValue;
-
-    // step-4: update current deposite amount
-    withdrawAmount.innerText = totalwithdrawAmount;
-
-    // step-5: get the value of balance amount and convert it to float type
+    // step-4: get the value of balance amount and convert it to float type
     const balanceAmount = document.getElementById('balance-amount');
     const balanceAmountValueString = balanceAmount.innerText;
     const balanceAmountValue = parseFloat(balanceAmountValueString);
 
-    // step-6: add input field value and balance amount
+    // extra step: error handling
+    if(withdrawFieldValue > balanceAmountValue) {
+        alert('Bap er bank e eto taka nai');
+        return;
+    }
+
+    // step-5: add input field value and deposite amount
+    const totalwithdrawAmount = withdrawFieldValue + withdrawAmountValue;
+
+    // step-6: update current deposite amount
+    withdrawAmount.innerText = totalwithdrawAmount;
+
+    // step-7: add input field value and balance amount
     const totalBalanceAmount = balanceAmountValue - withdrawFieldValue;
 
-    // step-7: update current balance amount
+    // step-8: update current balance amount
     balanceAmount.innerText = totalBalanceAmount;
-
-    // step-8: clear input field
-    withdrawField.value = ''
 })
+
+
+
